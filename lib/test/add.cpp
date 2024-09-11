1e02f2c563aa52226ae79ca6ab753553fa4730fe
X_Nefertar <xnefertar@gmail.com>
2024-09-11 22:37:34 +0800

    文件编译链接，引入动静态库概念

---
+++ lib/test/add.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "add.hpp"
+int add(int a, int b){
+    std::cout << "this is add func" << std::endl;
+    return a + b;
+}
---
+++ lib/test/sub.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "sub.hpp"
+int sub(int a, int b){
+    std::cout << "this is sub func" << std::endl;
+    return a - b;
+}
