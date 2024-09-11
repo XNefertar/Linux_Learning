c18cc7f089da4421240cd3477e5106eb8d607ff0
X_Nefertar <xnefertar@gmail.com>
2024-09-11 19:23:29 +0800

    添加了另一种显示环境变量的方法，并修复了一些问题

--- environ/env.c
+++ environ/env.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[], char* env[]){
-  printf("***********env*********\n");
+
+// Method 1
+// Through the command-line arguments
+  printf("***************** env ******************\n");
+  printf("************* Method 1 *******************");
   for(int i = 0; env[i]; ++i){
     printf("env[%d] = %s\n", i, env[i]);
   }
 
-  printf("***********argv*********\n");
-  for(int i = 0; i < argc; ++i){
-    printf("argv[%d] = %s\n", i, argv[i]);
+// Method 2
+// Through the third-party vatiable -- environ
+  printf("************* Method 2 *******************");
+  extern char** environ;
+  for(int i = 0; environ[i]; ++i){
+    printf("environ[%d] = %s\n", i, environ[i]);
   }
+
+//  printf("***********argv*********\n");
+//  for(int i = 0; i < argc; ++i){
+//    printf("argv[%d] = %s\n", i, argv[i]);
+//  }
 
   return 0;
 }
